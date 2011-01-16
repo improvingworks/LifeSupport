07f919e954340fa968547e69774ac1f5d1af85b5
Allen Hurst <allen@Macchiato.local>
2011-01-15 23:44:36 -0600

    adding template changes

--- wp-content/themes/twentyten/index.php
+++ wp-content/themes/twentyten/index.php
@@ -15,16 +15,25 @@
 
 get_header(); ?>
 
+hi
 		<div id="container">
-			<div id="content" role="main">
+			<div id="content" role="main">				
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-			<?php
-			/* Run the loop to output the posts.
-			 * If you want to overload this in a child theme then include a file
-			 * called loop-index.php and that will be used instead.
-			 */
-			 get_template_part( 'loop', 'index' );
-			?>
+					<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+
+					<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+
+				<?php endwhile; endif; ?>
+			
+			<div class="container_16">
+				get_post(18)->post_content;
+			</div>
+
+			<div class="container_16">
+				get_post();
+			</div>
+			
 			</div><!-- #content -->
 		</div><!-- #container -->
 
