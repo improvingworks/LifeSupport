cb2041ea4aa774ead048fee8fab7b8e36d8cd4ef
unknown <Michael@.(none)>
2011-01-15 18:01:23 -0600

    Adding plug in for adding widgets to pages and posts

---
+++ wp-content/plugins/add-widgets-to-page/addw2p.php
@@ -0,0 +1,165 @@
+<?php
+/*
+Plugin Name: add widgets to page
+Plugin URI: http://www.ie.u-ryukyu.ac.jp/~e065708/wp_plugins/addw2p.html
+Version: 1.2
+Description: Adds Wiget space to Posts and Pages using shortcode.
+Author: Christina Uechi
+Author URI: http://twitter.com/ucc_tina
+*/
+
+/*  Copyright 2009  Christina Uechi  ( tweet me : @ucc_tina )
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function addw2p_menu() {
+	if ( function_exists('add_management_page') ) {
+		add_management_page("addw2p", "Widgets to Pages", 8, __FILE__, 'addw2p_menu_options');
+	}
+}
+
+function addw2p_menu_options() {
+	echo '<div class="wrap">'."\n";
+	echo '<h2>Add Widgets to Page</h2>'."\n";
+	$names = get_option('addw2pdn');
+	$names = explode("|",$names);
+	//delete widgets.
+	if ( isset($_POST['Delete']) ) {
+		foreach( $names as $name) {
+			if ( $name != NULL ) $counts[] = $name;
+		}
+		echo "<p>Deleted";
+		foreach ( $counts as $delete ) {
+			if ( !isset($_POST[$delete]) ) {
+				$count [] = $delete;
+			} else {
+				echo " ".$delete;
+			}
+		}
+		foreach ( $count as $subname ) {
+			$new_names .= "|";
+			$new_names .= $subname;
+		}
+		update_option('addw2pdn',$new_names);
+		echo ".</p>\n";
+	//end deliting
+	} else { //count widget spaces
+		foreach( $names as $name) {
+			if ( $name != NULL ) $count[] = $name;
+		}
+	}
+	$num = count($count);
+	if ( $num <= 0 ) {//if no widget space
+		echo '<p>To use this, add [addw2p name="name"] to eny entries. (please change the name) (by using a same name, you are able to show same widget space)</p>'."\n";
+		echo "<p>There is no widget settings.</p></div>\n";
+		return;
+	}
+	//if there is a widgetspace
+?>
+<p>To use this, add [addw2p name="name"] to eny entries. (please change the name)<br />
+(by using a same name, you are able to show same widget space)</p>
+<p>To change the widgets that are showing, go to Appearance -> Widgets and edit the sidebar.</p>
+<p>To delete it, check the names which you want to delete and press Delete button below.</p>
+<p>There is <?php echo $num; ?> widget settings.</p>
+<form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+<table style="border:0;border-spacing:0;">
+<?php
+	wp_nonce_field('update-options');
+	foreach ($count as $name ) {
+		echo '<tr><td style="padding:5px 15px 5px 5px;"><input type="checkbox" name="'.$name.'" value ="'.$name.'" ></td>'."\n";
+		echo '<td style="padding:5px 15px 5px 5px;">'.$name.'</td></tr><br />'."\n";
+	}
+?>
+<tr><td colspan="2">
+<input type="hidden" name="action" value="update" />
+<input type="hidden" name="page_options" value="addw2pdn" />
+<p class="submit"><input type="submit" name="Delete" class="delete-opts" value="Delete" /></p>
+</td></tr>
+</table>
+</form>
+</div>
+<?php
+
+}
+
+function addw2p_css() {
+?>
+<style type="text/css">
+	.addw2p {
+		list-style-type: none;
+		list-style-image: none;
+	}
+</style>
+<?php
+}
+
+
+function addw2p_register($atts, $content) { //put widgets in enteries.
+	 extract(shortcode_atts(array(
+		'name' => '1'
+	), $atts));
+	if ( $name == 1) return $content;
+	$title = "addw2p-".$name;
+	$names = get_option('addw2pdn');
+	$names = explode("|",$names);
+	if( !in_array($name,$names) && $name != NULL ) {
+	    	array_push($names,$name);
+		foreach ($names as $subname ) {
+			$new_names .= "|";
+			$new_names .= $subname;
+		}
+		update_option('addw2pdn',$new_names);
+	}
+	//show widgets
+	ob_start();
+	echo '<ul class="ul-addw2p ul-'.$title.'">'."\n";
+	if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( $title ) ) :
+	   echo '<li>please set some widgets to show from Appearance -> Widgets.</li>'."\n";
+	endif;
+	echo "</ul>\n";
+	$myStr = ob_get_contents();
+	ob_end_clean();
+	return $myStr;
+}
+
+function addw2p_register_sidebar() {
+	$names = get_option('addw2pdn');
+	$names = explode("|",$names);
+	$num = count($names);
+	if ( $num <= 0 ) {
+		return;
+	}
+	foreach ( $names as $name ) {
+		if ( $name != NULL ) {
+			$title = "addw2p-".$name;
+			register_sidebar(array(
+				'before_widget' => '<li id="%1$s" class="addw2p %2$s">',
+				'after_widget' => '</li>',
+				'before_title' => '',
+				'after_title' => '',
+ 				'name' => $title
+ 			));
+ 		}
+	}
+	return;
+}
+
+add_action('wp_head','addw2p_css');
+add_shortcode('addw2p','addw2p_register');
+add_action('init','addw2p_register_sidebar');
+if ( is_admin() ) add_action('admin_menu','addw2p_menu');
+
+?>
---
+++ wp-content/plugins/add-widgets-to-page/readme.txt
@@ -0,0 +1,26 @@
+=== Add Widgets to Page ===
+Contributors: 
+Tags: addw2p
+Requires at least: 2.5
+Tested up to: 3.0
+
+Add Widgets to Page makes you able to add widget spaces like sidebar to posts and pages.
+
+== Description ==
+
+Add Widgets to Page makes you able to add widget spaces like sidebar to posts and pages.
+
+Write [addw2p name="name"] to any page or posts you want the widget space to show. Change the name to a unique name and you will be able to choose which widget you are going to use in Appearance -> Widgets. (by using a same name, you are able to show same widget space in different posts or pages)
+
+== Installation ==
+
+1. Push the folder that you downloaded into your wp-contet/plugins folder.
+2. Go to Plugin page on your WordPressSite, and activate it.
+
+== Changelog ==
+1.2 Now, the widget space will appear at the position of the short code.
+    Also checked if it works on 3.0.
+
+1.1 added css small bugfix
+
+1.0 
